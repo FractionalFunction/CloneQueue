5a54b9768699934bc04b51aafba6816cf6a84b81
Richie Zhang <richiezhang256@gmail.com>
2018-03-15 01:50:47 -0700

    Add the RepoURL textbox and the CloneRepo button.

--- CloneQueue/MainForm.Designer.vb
+++ CloneQueue/MainForm.Designer.vb
@@ -22,17 +22,41 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.RepoURL = New System.Windows.Forms.TextBox()
+        Me.CloneRepo = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'RepoURL
+        '
+        Me.RepoURL.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.RepoURL.Location = New System.Drawing.Point(22, 394)
+        Me.RepoURL.Name = "RepoURL"
+        Me.RepoURL.Size = New System.Drawing.Size(650, 35)
+        Me.RepoURL.TabIndex = 0
+        '
+        'CloneRepo
+        '
+        Me.CloneRepo.Location = New System.Drawing.Point(678, 393)
+        Me.CloneRepo.Name = "CloneRepo"
+        Me.CloneRepo.Size = New System.Drawing.Size(100, 45)
+        Me.CloneRepo.TabIndex = 1
+        Me.CloneRepo.Text = "Clone"
+        Me.CloneRepo.UseVisualStyleBackColor = True
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CloneRepo)
+        Me.Controls.Add(Me.RepoURL)
         Me.Name = "MainForm"
         Me.Text = "CloneQueue"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents RepoURL As TextBox
+    Friend WithEvents CloneRepo As Button
 End Class
--- CloneQueue/MainForm.vb
+++ CloneQueue/MainForm.vb
@@ -2,4 +2,12 @@
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub RepoURL_TextChanged(sender As Object, e As EventArgs) Handles RepoURL.TextChanged
+
+    End Sub
+
+    Private Sub CloneRepo_Click(sender As Object, e As EventArgs) Handles CloneRepo.Click
+
+    End Sub
 End Class
